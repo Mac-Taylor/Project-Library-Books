e85a0e420b66c8db6b399ab7788145065d6b8702
Mac Taylor <Mac@Macs-MacBook-Air.local>
2017-04-13 10:26:03 -0400

    initial JS commit

---
+++ librarybooks.js
@@ -0,0 +1,16 @@
+
+
+let books = [
+    {
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        price: 7.99,
+        cover: 'https://d35fkdjhhgt99.cloudfront.net/static/use-media-items/12/11893/full-1400x2136/56702545/The-Great-Gatsby.jpeg?resolution=0',
+    }
+]
+
+window.addEventListener('load', function (){
+
+    
+
+});
